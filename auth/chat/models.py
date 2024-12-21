a35881159aed5ad05a4f243c180bb0cd72ffdfb9
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2024-12-21 21:00:45 +0330

    Sprint4 chat was added without its settings

---
+++ auth/chat/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from users.models import User,Teacher,Student
+from django.core.exceptions import ValidationError
+
+class AccountForChat(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10, choices=[('manager', 'Manager'), ('teacher', 'Teacher'), ('student', 'Student')])
+    teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE, null=True, blank=True)
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, null=True, blank=True)
+
+    def clean(self):
+        if self.account_type == 'teacher' and self.student:
+            raise ValidationError('A teacher cannot be a student.')
+        if self.account_type == 'student' and self.teacher:
+            raise ValidationError('A student cannot be a teacher.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+class Chat(models.Model):
+    title = models.CharField(max_length=200)
+    participants = models.ManyToManyField(AccountForChat, related_name='chats')
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(AccountForChat, on_delete=models.CASCADE, related_name='sent_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
