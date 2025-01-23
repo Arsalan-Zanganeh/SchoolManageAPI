71c70969d84102d658430567b91114f197206941
Arsalan-Zanganeh <a_doost@comp.iust.ac.ir>
2025-01-23 10:15:42 +0330

    off sprint student picture view policy changed

--- auth/portfolio/views.py
+++ auth/portfolio/views.py
@@ -60,43 +60,37 @@ class FilterStudents(APIView):
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
 
+
 class StudentPictureTeacherSideView(APIView):
     def post(self, request):
-        token = request.COOKIES.get('jwt')
-
+        token = request.COOKIES.get('school') or request.COOKIES.get('jwt')
         if not token:
             raise AuthenticationFailed("Unauthenticated!")
 
         try:
-            payload = jwt.decode(token, 'django-insecure-7sr^1xqbdfcxes^!amh4e0k*0o2zqfa=f-ragz0x0v)gcqx121', algorithms=['HS256'])
+            payload = jwt.decode(token, 'django-insecure-7sr^1xqbdfcxes^!amh4e0k*0o2zqfa=f-ragz0x0v)gcqx121',
+                                 algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed("Expired token!")
 
-        teacher = Teacher.objects.filter(National_ID=payload['National_ID']).first()
-        if not teacher:
-            raise AuthenticationFailed("There is no teacher with this National_ID")
+        if 'Postal_Code' in payload:
+            school = School.objects.filter(Postal_Code=payload['Postal_Code']).first()
+            if not school:
+                raise AuthenticationFailed("School not found!")
 
-        token = request.COOKIES.get('class')
+        elif 'National_ID' in payload:
+            teacher = Teacher.objects.filter(National_ID=payload['National_ID']).first()
+            if not teacher:
+                raise AuthenticationFailed("There is no teacher with this National_ID")
 
-        if not token:
-            raise AuthenticationFailed("Unauthenticated!")
-
-        try:
-            payload = jwt.decode(token, 'django-insecure-7sr^1xqbdfcxes^!amh4e0k*0o2zqfa=f-ragz0x0v)gcqx121', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed("Expired token!")
-
-
-        myclass = Classes.objects.filter(id=payload['Class_ID']).first()
-        if not myclass:
-            return Response({"message":"There is no class with this ID"})
-
-        students1 = ClassStudent.objects.filter(Classes=myclass, Student__National_ID=request.data['National_ID']).first()
-
+        students1 = Student.objects.filter(National_ID=request.data['National_ID']).first()
         if not students1:
-            raise AuthenticationFailed("There is no student with this National_ID in your class")
+            raise AuthenticationFailed("There is no student with this National_ID")
 
-        profPic = StudentProfile.objects.filter(student=students1.Student).first()
+        profPic = StudentProfile.objects.filter(student=students1).first()
+        if not profPic:
+            return Response({"message": "No profile picture found for this student."}, status=404)
+
         serializer = StudentPictureSerializer(profPic)
         return Response(serializer.data)
 
